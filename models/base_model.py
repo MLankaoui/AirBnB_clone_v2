bebe16d43292d80b6f0ac5bed617c0202d19d1be
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-24 13:39:36 +0100

    added .gitginore and changed self.id and other object variables and imported sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,10 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String,DateTime, func
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -10,10 +13,10 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = Column(String(60), primary_key=True, nullable=False)
+            self.created_at = Column(DateTime, nullable=False, default=func.utcnow)
+            self.updated_at = Column(DateTime, nullable=False, default=func.utcnow)
+            storage.save(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
