0bb92f801c691b9b5ae1bd511fe80af899641c8b
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-25 15:26:43 +0100

    fixing more bugs

--- models/city.py
+++ models/city.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-""" City Module for HBNB project. """
-from models.base_model import BaseModel
+"""This is the city class"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from models.place import Place
 
 
 class City(BaseModel, Base):
-    """ City class representing cities in the application. """
-    __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    places = relationship("Place", cascade="all, delete, delete-orphan", backref="citites")
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
