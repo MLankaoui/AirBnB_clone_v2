dd15916c367c537d719e5d094616460778e8d277
MLankaoui <marouanelankaoui66@gmail.com>
2024-06-25 15:13:14 +0100

    finished task 6 hopefully

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -12,3 +13,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="all, delete, delete-orphan", backref="citites")
--- models/state.py
+++ models/state.py
@@ -1,13 +1,44 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import shlex
+import models
 from models.base_model import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 Base = declarative_base()
+STORAGE_TYPE = os.environ.get("HBNB_TYPE_STORAGE")
+
 
 
 class State(BaseModel, Base):
     """ State class representing states in the application. """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+    
+
+    @property
+    def cities(self):
+        if STORAGE_TYPE != 'db':
+            v = models.storage.all()
+            ls = {}
+            rs = []
+
+            for k in v:
+                city = k.replace('.', ' ')
+                city = shlex.split(city)
+
+                if city[0] == 'City':
+                    ls.append(v[k])
+            for el in ls:
+                if el.state_id == self.id:
+                    rs.append(el)
+
+            return rs
+        
+        else:
+            return [city for city in self.citites]
