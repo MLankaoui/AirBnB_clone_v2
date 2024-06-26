a4bdcc5164b81d6186a1fb7e6b31445bdb99bb9f
Fatima20027 <fatima.hailou@icloud.com>
2024-06-26 21:23:20 +0100

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+
+Base = declarative_base()
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    Class for Amenity
+    Attribute:
+        name = input name
+    """
+    __tablename__ = 'amenities'
+    name = Column(str(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity)
